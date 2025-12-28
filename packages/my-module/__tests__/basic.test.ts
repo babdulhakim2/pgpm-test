8ce14c436c1ace0eb846844ed78b22b777931036
Abdul <abdulhakim.gafai@gmail.com>
2025-12-28 15:07:08 -0800

    fix-up

--- packages/my-module/__tests__/basic.test.ts
+++ packages/my-module/__tests__/basic.test.ts
@@ -22,14 +22,17 @@ afterEach(async () => {
 
 it("should pass", async () => {
   const result = await pg.query(
-    "SELECT faker.word() as word, faker.name() as name"
+    "SELECT faker.word() as word, faker.name() as name, faker.email() as email"
   );
   console.log(result.rows);
+  expect(result.rows[0].word).toBeDefined();
+  expect(result.rows[0].name).toBeDefined();
+  expect(result.rows[0].email).toBeDefined();
 });
 
 it("should create a pet", async () => {
   const result = await pg.query(
-    "INSERT INTO my_pets.pets (name, age) VALUES ($1, $2) RETURNING *`",
+    "INSERT INTO my_pets.pets (name, age) VALUES ($1, $2) RETURNING *",
     ["Fido", 3]
   );
   console.log(result.rows);
